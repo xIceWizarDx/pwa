73e10654dd45b71660a5268728319ecc6f934d52
gabrielmoraes102roo@gmail.com <96112719Gg.>
2025-07-02 22:05:09 -0400

    Atualização

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import ExampleComponent from './components/ExampleComponent.vue';
+
+createApp({}).component('example-component', ExampleComponent).mount('#app');
+
