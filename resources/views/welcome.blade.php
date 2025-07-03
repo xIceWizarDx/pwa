2aed5dcccf378abf2d461533afbcbb9c6f618d19
gabrielmoraes102roo@gmail.com <96112719Gg.>
2025-07-02 23:11:04 -0400

    Atualização

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,23 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Controle Financeiro</title>
+    <!-- Inclui CSS e JS compilados pelo Vite -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+
 <body class="antialiased">
+    <!-- Container principal do Vue -->
     <div id="app">
-        <wellcomee />
+        <!-- Componente de boas-vindas -->
+        <wellcomee></wellcomee>
+
+        <!-- Componente de controle financeiro -->
+        <finance-control></finance-control>
     </div>
 </body>
+
 </html>
