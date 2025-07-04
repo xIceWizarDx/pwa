e5bd98d01ab931e2bcc251b7a25830b404cea780
xIceWizarDx <99181764+xIceWizarDx@users.noreply.github.com>
2025-07-04 19:41:48 -0400

    Configure Tailwind plugin for Vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
     plugins: [
@@ -9,6 +10,7 @@ export default defineConfig({
             refresh: true,
         }),
         vue(),
+        tailwindcss(),
     ],
     resolve: {
         alias: {
